25715d4e917f1532add910ee62fbfc83614ddd14
lucasrandisi <lucasrandisi@gmail.com>
2020-12-08 20:53:43 -0300

    Menu tables en el sidenav

--- src/components/common/nav/SidebarRoutes.tsx
+++ src/components/common/nav/SidebarRoutes.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import {MdRestaurantMenu} from "react-icons/md";
-import {FiHome} from "react-icons/fi";
-import {BiBookAdd} from "react-icons/bi";
-import {BsGear} from "react-icons/bs";
+import { MdRestaurantMenu } from "react-icons/md";
+import { FiHome } from "react-icons/fi";
+import { BiBookAdd } from "react-icons/bi";
+import { BsGear } from "react-icons/bs";
+import { AiFillAppstore } from "react-icons/ai";
 
 const SidebarRoutes = [
 	{
 		title: "Home",
 		path: "/",
 		icon: <FiHome />,
+	},
+	{
+		title: "Tables",
+		path: "/mesas",
+		icon: <AiFillAppstore />,
 	},
 	{
 		title: "Reservations",
