9378773cadaeaa3454ef8657417f0e842d12516d
lucasrandisi <lucasrandisi@gmail.com>
2021-01-10 21:06:50 -0300

    Theme

---
+++ src/styles/theme.tsx
@@ -0,0 +1,12 @@
+export const theme = {
+	color1: "#132261",
+	color2: "#e6e6e6",
+	color3: "#08b867",
+	color4: "#ffffff",
+	color5: "#e8b56a",
+
+	sm: "320px",
+	md: "768px",
+	lg: "1366px",
+	xl: "1920px",
+};
