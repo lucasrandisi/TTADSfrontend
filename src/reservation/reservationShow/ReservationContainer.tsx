f71f4af0f1c8bb4e03437c425033bc0ab4e2e028
Natalia Fernandez <natalia77@live.com>
2021-03-04 19:00:18 -0300

    reservation filters

--- src/reservation/reservationShow/ReservationContainer.tsx
+++ src/reservation/reservationShow/ReservationContainer.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { Grid, Container } from "@material-ui/core";
 import { useQuery } from "@apollo/client";
+import moment from "moment";
 import ReservationTable from "./reservationTable/ReservationTable";
+import ReservationFilters from "./ReservationFilters";
 import { GET_RESERVATIONS } from "../queries/ReservationQuery";
 import ReservationButtonNew from "./ReservationButtonNew";
 import ReservationPagination from "./ReservationPagination";
 
 export default function Reservations() {
+	const [searchInput, setSearchInput] = useState("");
 	const [currentPage, setCurrentPage] = useState(1);
 
 	const { data, loading, error } = useQuery(GET_RESERVATIONS);
@@ -18,16 +21,32 @@ export default function Reservations() {
 	const indexLastRes = currentPage * resPerPage;
 	const indexFirstRes = indexLastRes - resPerPage;
 
+	const filterSearch = () => {
+		if (!searchInput) {
+			return reservations;
+		}
+
+		return reservations.filter(
+			r =>
+				r.customerName.toLowerCase().includes(searchInput.toLowerCase()) ||
+				moment(r.reservationDateTime)
+					.format("DD/MM/YYYY HH:MM")
+					.toString()
+					.includes(searchInput)
+		);
+	};
+
 	return (
 		<>
 			<Container component="main" maxWidth="lg">
 				<Grid container spacing={3}>
 					<ReservationButtonNew />
+					<ReservationFilters searchInput={searchInput} setSearchInput={setSearchInput} />
 					<ReservationTable
-						reservations={reservations.slice(indexFirstRes, indexLastRes)}
+						reservations={filterSearch().slice(indexFirstRes, indexLastRes)}
 					/>
 					<ReservationPagination
-						lenReservations={reservations.length}
+						lenReservations={filterSearch().length}
 						page={resPerPage}
 						currentPage={currentPage}
 						setCurrentPage={setCurrentPage}
