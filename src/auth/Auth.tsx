631764350e7aef4eac4a49fdbaf6f7fbe41051f4
FernandezNatalia <natalia77@live.com>
2022-10-07 11:55:20 -0300

    auth login and register

---
+++ src/auth/Auth.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import RegisterForm from "./register-form/RegisterForm";
+import logo from "../assets/png/Restaurant.png";
+import "./Auth.scss";
+
+export default function Auth() {
+	const [showLogin, setShowLogin] = useState(false);
+
+	return (
+		<Container fixed className="auth">
+			<img alt="undefined" className="img-form" src={logo} />
+
+			<div className="container-form">
+				{showLogin ? <p>form de login</p> : <RegisterForm setShowLogin={setShowLogin} />}
+			</div>
+
+			<div className="change-form">
+				<p>
+					{showLogin ? (
+						<>
+							¿No tienes una cuenta?
+							{/* eslint-disable-next-line */}
+							<span onClick={() => setShowLogin(!showLogin)}>Crea una</span>
+						</>
+					) : (
+						<>
+							¿Ya tienes una cuenta?
+							{/* eslint-disable-next-line */}
+							<span onClick={() => setShowLogin(!showLogin)}>Iniciar sesión</span>
+						</>
+					)}
+				</p>
+			</div>
+		</Container>
+	);
+}
